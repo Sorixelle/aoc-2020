1f7e1cabb1d6fe4135e0a92a895c94db79843e22
Ruby Iris Juric <ruby@srxl.me>
2020-12-01 23:49:16 +1100

    Day 1: Report Repair - Nicer implementation

--- 01ReportRepair.hs
+++ 01ReportRepair.hs
@@ -17,10 +17,10 @@ findResult = either id id . findResult'
 
 findResult' :: [String] -> Either Int Int
 findResult' strings =
-  foldM f 0 numbers
+  foldM f 0 pairs
   where
     numbers = map (\s -> read s :: Int) strings
-    f _ n = foldM (f' n) 0 numbers
-    f' cur _ n
-      | cur + n == 2020 = Left (cur * n)
-      | otherwise       = Right 0
+    pairs = sequence $ replicate 2 numbers
+    f acc p
+      | (sum p) == 2020 = Left (product p)
+      | otherwise       = Right acc
