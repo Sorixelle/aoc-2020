d4b6f2a128e41fe5154b902d3971bb8ff96e2d19
Ruby Iris Juric <ruby@srxl.me>
2020-12-01 23:57:03 +1100

    Day 1: Report Repair - Completed Part 2

--- 01ReportRepair.hs
+++ 01ReportRepair.hs
@@ -10,17 +10,18 @@ run :: String -> IO ()
 run fileName = do
   contents <- readFile fileName
   let fileLines = lines contents
-  putStrLn $ "Result: " ++ (show $ findResult fileLines)
+  putStrLn $ "Part 1 Result: " ++ (show $ findResult 2 fileLines)
+  putStrLn $ "Part 2 Result: " ++ (show $ findResult 3 fileLines)
 
-findResult :: [String] -> Int
-findResult = either id id . findResult'
+findResult :: Int -> [String] -> Int
+findResult pairLength = either id id . (findResult' pairLength)
 
-findResult' :: [String] -> Either Int Int
-findResult' strings =
+findResult' :: Int -> [String] -> Either Int Int
+findResult' pairLength strings =
   foldM f 0 pairs
   where
     numbers = map (\s -> read s :: Int) strings
-    pairs = sequence $ replicate 2 numbers
+    pairs = sequence $ replicate pairLength numbers
     f acc p
       | (sum p) == 2020 = Left (product p)
       | otherwise       = Right acc
