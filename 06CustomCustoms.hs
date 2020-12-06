fca7c7e373936987b6a270c2a2e7c66f0f002bc7
Ruby Iris Juric <ruby@srxl.me>
2020-12-06 18:09:11 +1100

    Day 6: Custom Customs - Completed Part 2

--- 06CustomCustoms.hs
+++ 06CustomCustoms.hs
@@ -3,15 +3,21 @@ import System.Environment
 
 splitAtEmptyLine :: [String] -> String -> [String]
 splitAtEmptyLine xs "" = "" : xs
-splitAtEmptyLine (x:xs) s = (x ++ s) : xs
+splitAtEmptyLine ("":xs) s = s : xs
+splitAtEmptyLine (x:xs) s = (x ++ " " ++ s) : xs
 splitAtEmptyLine xs s = [s]
+
+findCommonAnswers :: String -> String
+findCommonAnswers xs = foldl1 intersect $ nub <$> words xs
 
 run :: String -> IO ()
 run fileName = do
   contents <- readFile fileName
   let groups = foldl' splitAtEmptyLine [] $ lines contents
-  let sumOfAnswers = sum $ (length . nub) <$> groups
+  let sumOfAnswers = sum $ (length . nub) <$> filter (/= ' ') <$> groups
   putStrLn $ "Sum of amount of answers (Part 1): " ++ (show sumOfAnswers)
+  let sumOfCommonAnswers = sum $ length <$> findCommonAnswers <$> groups
+  putStrLn $ "Sum of group common answers (Part 2): " ++ (show sumOfCommonAnswers)
 
 main :: IO ()
 main = do
