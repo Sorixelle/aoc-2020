1fbd5f37aa246246c5b2c61028dfb0769d938e1f
Ruby Iris Juric <ruby@srxl.me>
2020-12-13 16:02:24 +1100

    Day 12: Rain Risk - Completed Part 2

--- 12RainRisk.hs
+++ 12RainRisk.hs
@@ -2,13 +2,7 @@ import Data.List
 import System.Environment
 
 data Ship = Ship Int Int Int deriving Show
-
-degToDirection :: Int -> Char
-degToDirection deg
-  | deg == 0   = 'N'
-  | deg == 90  = 'E'
-  | deg == 180 = 'S'
-  | deg == 270 = 'W'
+data Waypoint = Waypoint Int Int deriving Show
 
 followRoute :: Ship -> String -> Ship
 followRoute ship@(Ship x y r) instruction = case head dir of
@@ -17,13 +11,37 @@ followRoute ship@(Ship x y r) instruction = case head dir of
   'E' -> Ship (x + amt) y r
   'W' -> Ship (x - amt) y r
   'F' -> followRoute ship ((degToDirection r):val)
-  'L' -> Ship x y $ rotateCCW amt
-  'R' -> Ship x y $ rotateCW amt
+  'L' -> Ship x y $ rotate (-amt)
+  'R' -> Ship x y $ rotate amt
   where
     (dir, val) = splitAt 1 instruction
     amt = read val
-    rotateCW d = abs $ (r + d) `mod` 360
-    rotateCCW d = abs $ (r - d) `mod` 360
+    rotate d = abs $ (r + d) `mod` 360
+    degToDirection deg
+      | deg == 0   = 'N'
+      | deg == 90  = 'E'
+      | deg == 180 = 'S'
+      | deg == 270 = 'W'
+
+followWaypoint :: (Waypoint, Ship) -> String -> (Waypoint, Ship)
+followWaypoint (wp@(Waypoint wx wy), ship@(Ship x y r)) instruction = case head dir of
+  'N' -> (Waypoint wx (wy + amt), ship)
+  'S' -> (Waypoint wx (wy - amt), ship)
+  'E' -> (Waypoint (wx + amt) wy, ship)
+  'W' -> (Waypoint (wx - amt) wy, ship)
+  'F' -> (wp, Ship (x + (wx * amt)) (y + (wy * amt)) r)
+  'L' -> (rotateWaypoint wp (-amt), ship)
+  'R' -> (rotateWaypoint wp amt, ship)
+  where
+    (dir, val) = splitAt 1 instruction
+    amt = read val
+    rotateWaypoint (Waypoint x y) r
+      | deg == 0   = Waypoint x y
+      | deg == 90  = Waypoint y (-x)
+      | deg == 180 = Waypoint (-x) (-y)
+      | deg == 270 = Waypoint (-y) x
+      where
+        deg = abs $ (360 + r) `mod` 360
 
 manhattanDistance :: Ship -> Int
 manhattanDistance (Ship x y _) = (abs x) + (abs y)
@@ -32,8 +50,10 @@ run :: String -> IO ()
 run fileName = do
   contents <- readFile fileName
   let instructions = lines contents
-  let finalPos = foldl' followRoute (Ship 0 0 90) instructions
-  putStrLn $ "Manhattan distance from ship starting location (Part 1): " ++ (show $ manhattanDistance finalPos)
+  let finalPosP1 = foldl' followRoute (Ship 0 0 90) instructions
+  putStrLn $ "Manhattan distance from ship starting location (Part 1 rules): " ++ (show $ manhattanDistance finalPosP1)
+  let finalPosP2 = snd $ foldl' followWaypoint (Waypoint 10 1, Ship 0 0 0) instructions
+  putStrLn $ "Manhattan distance from ship starting location (Part 2 rules): " ++ (show $ manhattanDistance finalPosP2)
 
 main :: IO ()
 main = do
